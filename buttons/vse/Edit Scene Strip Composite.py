6ec803fbfb92e4337594faf1c4a4579f5cdafc1a
Hudson Barkley <snuq@snuq.com>
2019-03-06 22:35:20 -0800

    Started work on 2.8 port

--- buttons/vse/Edit Scene Strip Composite.py
+++ buttons/vse/Edit Scene Strip Composite.py
@@ -7,8 +7,8 @@ sequencer = scene.sequence_editor
 strip = sequencer.active_strip
 
 strip_scene = strip.scene
-screens = [screen for screen in bpy.data.screens.keys() if 'compositing' in screen.lower()]
-if len(screens) >= 1:
-    screen = bpy.data.screens[screens[0]]
-    bpy.context.window.screen = screen
-    screen.scene = strip_scene
+workspaces = [workspace for workspace in bpy.data.workspaces.keys() if 'compositing' in workspace.lower()]
+if len(workspaces) >= 1:
+    workspace = bpy.data.workspaces[workspaces[0]]
+    bpy.context.window.workspace = workspace
+    bpy.context.window.scene = strip_scene
